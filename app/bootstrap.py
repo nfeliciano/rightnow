6efea8d6f8bd033fd1d30aff03c3de01049ad80b
drusk <drusk@uvic.ca>
2016-03-12 19:49:01 -0800

    bootstrap.py now runs the restaurant scraper.

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -1,4 +1,7 @@
 from database import init_db
+import scrape_restaurants
+
 
 if __name__ == '__main__':
     init_db()
+    scrape_restaurants.run_scraper()
--- app/scrape_restaurants.py
+++ app/scrape_restaurants.py
@@ -7,7 +7,7 @@ import models
 TOURISM_VICTORIA_RESTAURANTS_ENDPOINT = "http://www.tourismvictoria.com/includes/cfcs/listings/listings_ajax.cfm?menuid=1255&HASDTN=1&MAXSHOW=162&SORTDIR=ASC&SHOWMAP=true&TOTALRESULTS=162&STARTROW=1&SORTBY=dtnRank%20DESC,%20rankid%20ASC,%20sortCompany%20ASC&USERANK=1&SUBCATID=0&DEPARTUREID=0&SFILTER=ALL&CATID=1806&RATES=0&EFILTER=ALL&ITINERARYTHEMEID=0&LISTIDS=0&REGIONID=0&startRow=11"
 
 
-def main():
+def run_scraper():
     response = requests.get(TOURISM_VICTORIA_RESTAURANTS_ENDPOINT)
 
     restaurants = []
@@ -33,4 +33,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    run_scraper()
