e62b8173f63e3257219e8bab83655269c647735c
hirundoRustica <hirundoRustica@users.noreply.github.com>
2016-03-13 03:44:20 -0600

    events

--- app/scrape_events.py
+++ app/scrape_events.py
@@ -4,7 +4,7 @@ import database
 import models
 
 
-TOURISM_VICTORIA_EVENTS_ENDPOINT = "https://www.eventbrite.ca/directory/json?page=&cat=&format=&q=&loc=Victoria%2C+BC%2C+Canada&date=&start_date=&end_date=&is_paid=&sort=best&crt=regular&slat=&slng=&radius=&vp_ne_lat=&vp_ne_lng=&vp_sw_lat=&vp_sw_lng=&view=list"
+TOURISM_VICTORIA_EVENTS_ENDPOINT = "https://www.eventbrite.ca/directory/json?page={}&cat=&format=&q=victoria+bc&loc=Victoria%2C+BC%2C+Canada&date=&start_date=&end_date=&is_paid=&sort=best&crt=regular&slat=&slng=&radius=&vp_ne_lat=&vp_ne_lng=&vp_sw_lat=&vp_sw_lng=&view=list"
 
 def parse_datetime(date_string):
    return datetime.datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S")
@@ -15,10 +15,8 @@ def ticket_availability(m_placemark):
     else:
         return (m_placemark["ticket_availability"]["remaining_capacity"] > 0)
 
-
-def run_scraper():
-    response = requests.get(TOURISM_VICTORIA_EVENTS_ENDPOINT)
-
+def parse_events(EVENTS_ENDPOINT):
+    response = requests.get(EVENTS_ENDPOINT)
     events = []
     for placemark in response.json()["events"]:
         if placemark["venue"]["address"]["city"] == "Victoria":
@@ -42,6 +40,16 @@ def run_scraper():
     print "Adding {} events to database".format(len(events))
     database.db_session.add_all(events)
     database.db_session.commit()
+
+def run_scraper():
+    response = requests.get(TOURISM_VICTORIA_EVENTS_ENDPOINT.format(""))
+    page_count = response.json()["pagination"]["page_count"]
+
+    parse_events(TOURISM_VICTORIA_EVENTS_ENDPOINT.format(""))
+    if page_count > 1:
+        page_number = 2
+        while page_number < page_count:
+            parse_events(TOURISM_VICTORIA_EVENTS_ENDPOINT.format("page_number"))
 
 
 if __name__ == "__main__":
