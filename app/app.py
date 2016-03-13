a5b1a994f26b5a6250000b379c38946baaa638a0
drusk <drusk@uvic.ca>
2016-03-12 19:37:03 -0800

    Added function to recommend a random restaurant from the database.

--- app/app.py
+++ app/app.py
@@ -1,5 +1,19 @@
+import random
+
 from flask import Flask, send_file
+
+import database
+import models
+
+
 app = Flask(__name__, static_url_path='/static')
+
+
+def recommend_restaurant():
+    # To start with just recommend a restaurant at random
+    random_index = random.randrange(0, database.db_session.query(models.Restaurant).count())
+    return database.db_session.query(models.Restaurant)[random_index]
+
 
 @app.route('/')
 def index():
