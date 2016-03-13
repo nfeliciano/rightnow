d8bdb548b1943772298cdc435f08432bfa6290aa
nfeliciano <feliciano.noel@gmail.com>
2016-03-12 19:10:30 -0800

    having difficulty

--- app/static/js/formController.js
+++ app/static/js/formController.js
@@ -1,11 +1,20 @@
 rightnow.controller('formController', function($scope) {
   $scope.formData = {};
+  $scope.selectedTime = { name: 'None' };
+
+  $scope.times = [
+    { name: 'Right Now!' },
+    { name: 'Later Tonight' },
+    { name: 'Tomorrow' },
+    { name: 'This Friday' },
+    { name: 'This Saturday' }
+  ];
 
   $scope.findEvent = function() {
     alert('FIND SOMETHING');
   }
 
   $scope.timeSelected = function() {
-    alert('selected time');
+    console.log($scope.selectedTime)
   }
 });
