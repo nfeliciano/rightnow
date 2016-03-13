3125514ff65b392b8dc45a214b5bc01fc44c7010
Richard B. Wagner <breckwagner@gmail.com>
2016-03-12 18:29:03 -0800

    Added setup script

---
+++ app/README.md
@@ -0,0 +1,20 @@
+
+
+# Bootstrap
+
+
+
+Ubuntu:
+```bash
+& PYTHON_ENV_FOR_APP=venv
+$ virtualenv $(PYTHON_ENV_FOR_APP)
+$ . $(PYTHON_ENV_FOR_APP)/bin/activate
+$ pip install -r requirements.txt
+$ python bootstrap.py
+```
+
+# Running
+
+```bash
+$ python app.py
+```
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from database import Base
 class Event(Base):
     __tablename__ = 'events'
     id = Column(Integer, primary_key=True)
+    name = Column(String(512), unique=False)
     atPlace = Column(String(512), unique=False)
     atTime = Column(DateTime, unique=False)
     circa = Column(DateTime, unique=False)
@@ -13,10 +14,32 @@ class Event(Base):
     involvedAgent = Column(String(512), unique=False)
     url = Column(String(2083), unique=True)
 
-
-    def __init__(self, name=None, email=None):
+    def __init__(self, name=None, atPlace=None, atTime=None, circa=None, illustrate=None, inSpace=None, involved=None, involvedAgent=None, url=None):
         self.name = name
-        self.email = email
+        self.atPlace = atPlace
+        self.atTime = atTime
+        self.circa = circa
+        self.illustrate = illustrate
+        self.inSpace = inSpace
+        self.involved = involved
+        self.involvedAgent = involvedAgent
+        self.url = url
 
     def __repr__(self):
-        return '<User %r>' % (self.name)
+        return '<Event %r>' % (self.name)
+
+
+
+'''
+
+name
+address
+description
+image (URL)
+latitude
+longitude
+phone number
+subcategory (ex: desert, bar)
+website
+postal code
+'''
